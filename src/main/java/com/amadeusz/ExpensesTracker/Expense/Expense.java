e02cb5848142b98118b601f6bc73763041989f8b
Taito99 <amadeuszbujalski300@gmail.com>
2024-12-15 21:09:23 +0100

    Second Intial Commit

--- src/main/java/com/amadeusz/ExpensesTracker/Expense/Expense.java
+++ src/main/java/com/amadeusz/ExpensesTracker/Expense/Expense.java
@@ -1,4 +1,26 @@
 package com.amadeusz.ExpensesTracker.Expense;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Expense {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+
 }
